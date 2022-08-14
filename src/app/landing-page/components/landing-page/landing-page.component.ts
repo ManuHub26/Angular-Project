344cb9ac5a6e582bdf89dc8e4dc9c17a6f476cf8
Emmanuel <emmanuel.cohen00@gmail.com>
2022-08-14 17:12:18 +0200

    add module structure

--- src/app/landing-page/components/landing-page/landing-page.component.ts
+++ src/app/landing-page/components/landing-page/landing-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { from } from 'rxjs';
 
 @Component({
   selector: 'app-landing-page',
